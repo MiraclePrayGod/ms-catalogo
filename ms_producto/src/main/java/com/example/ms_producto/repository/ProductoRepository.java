61d208850f8d72d77c0cbe6bef7af04e1b2787ca
Dan <reginaldobuendia@gmail.com>
2025-04-03 09:26:25 -0500

    Filtro solucioonado

--- ms_producto/src/main/java/com/example/ms_producto/repository/ProductoRepository.java
+++ ms_producto/src/main/java/com/example/ms_producto/repository/ProductoRepository.java
@@ -1,8 +1,4 @@
 package com.example.ms_producto.repository;
 
-import com.example.ms_producto.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer>{
-
+public interface ProductoRepository {
 }
